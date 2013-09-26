9ebaaf3f90ef2fa0a1d0ac924f92cf8f349e5f24
zelgerj <zelger@me.com>
2013-09-26 19:07:23 +0200

    added app view link on overview

--- app/scripts/controllers/app_controller.js
+++ app/scripts/controllers/app_controller.js
@@ -1,9 +1,13 @@
 Appserver.AppController = Ember.ObjectController.extend({
 
-    removeVhost : function() {
+    removeApp : function() {
         var app = this.get('model');
         app.deleteRecord();
         app.save();
     },
+
+    url : function () {
+		return "http://localhost:8586/" + this.get('name');
+	}.property()
 
 });
