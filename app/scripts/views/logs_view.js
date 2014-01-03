64628aace635a6b57785e85286fa10cb00db91d8
Johann Zelger <zelger@me.com>
2014-01-03 16:42:45 +0100

    added websocket for log area

---
+++ app/scripts/views/logs_view.js
@@ -0,0 +1,28 @@
+/**
+ * The live log view with websockets
+ *
+ * @class Appserver.LogsView
+ * @extends Ember.View
+ */
+Appserver.LogsView = Ember.View.extend({
+
+    /**
+     * Initialises the dropzone when ember rendering is finished
+     *
+     * @method didInsertElement
+     * @return {Object}
+     */
+    didInsertElement: function () {
+        // init websocket connection
+        var logWebsocket = new WebSocket(
+            Appserver.websocketUrl + '/admin/logging'
+        );
+        // display data on message on content
+        logWebsocket.onmessage = function (evt) {
+            Ember.$('#liveLogContent').prepend(
+                evt.data + "</br>"
+            );
+        };
+    }
+
+});
